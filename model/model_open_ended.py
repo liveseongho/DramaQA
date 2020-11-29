[bugfix] Fixing initialization error and averaging loss
Fixing initialization error and averaging loss

model/model_open_ended.py
@@ -169,7 +169,7 @@ class OpenEnded(BaseModel):
     
         use_teacher_forcing = True if random.random() < self.teacher_forcing_ratio else False
        
-        decoder_outputs = torch.empty(target_length, 1, self.V).to(self.device)
+        decoder_outputs = torch.zeros(target_length, 1, self.V).to(self.device)
         if use_teacher_forcing:
             for di in range(target_length):
                 decoder_output, decoder_hidden, decoder_attention = self.decoder(

trainer/trainer.py
@@ -56,6 +56,7 @@ class Trainer(BaseTrainer):
                 loss += self.criterion(output[di] ,target[di])
                 topv, topi = output[di].topk(1)
                 pred.append(topi.item())
+            loss = loss / len(target)
             pred = torch.tensor(pred)
             target = target.transpose(0, 1)
             target = target.squeeze(0)
@@ -64,7 +65,6 @@ class Trainer(BaseTrainer):
             self.optimizer.step()
             self.writer.set_step((epoch - 1) * self.len_epoch + batch_idx)
             self.train_metrics.update('loss', loss.item())
-            
             
             for met in self.metric_ftns:
                 if 'accuracy_diff' in met.__name__:
@@ -122,6 +122,7 @@ class Trainer(BaseTrainer):
                     loss += self.criterion(output[di] ,target[di])
                     topv, topi = output[di].topk(1)
                     pred.append(topi.item())
+                loss = loss / len(target)
                 pred = torch.tensor(pred)
                 target = target.transpose(0, 1)
                 target = target.squeeze(0)