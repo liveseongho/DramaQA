b335a3ab5c26cd0a513c11f6f97dd30205fc6ab4
leedonggeon <donggeonlee.v@gmail.com>
2021-03-04 21:50:17 +0900

    [feat] bert_score, bleurt impl
    bert_score, bleurt impl

--- model/metric_gpt.py
+++ model/metric_gpt.py
@@ -67,3 +67,52 @@ def Cider(output, target, tokenizer):
 #    return score
 
     return 0
+
+from bert_score import BERTScorer
+
+bert_scorer = BERTScorer(lang="en", rescale_with_baseline=True)
+
+def BertScore(output,target, tokenizer):
+
+    pred_sentence = to_string(output, tokenizer)
+    target_sentence = to_string(target, tokenizer)
+
+    pred_sentence = [' '.join(pred_sentence)]
+    target_sentence = [' '.join(target_sentence)]
+
+    P, R, F1 = bert_scorer.score(pred_sentence, target_sentence)
+
+    # P, R, F = bert_score.score(pred_sentence, target_sentence, lang="en")
+
+    '''params = bert_score.score_model(pred_sentence, target_sentence, lang='en')
+
+    from pprint import pprint
+
+    print('params:', len(params))
+    pprint(params)
+
+    P, R, F1 = bert_score.bert_score(params)'''
+
+    return F1[0].item()
+
+
+from .bleurt.bleurt import score as bleurt_score
+
+from .bleurt.bleurt.score import bleurt_checkpoint
+
+
+bleurt_scorer = bleurt_score.BleurtScorer(bleurt_checkpoint)
+
+def BleuRT(output, target, tokenizer):
+
+    pred_sentence = to_string(output, tokenizer)
+    target_sentence = to_string(target, tokenizer)
+
+    pred_sentence = [' '.join(pred_sentence)]
+    target_sentence = [' '.join(target_sentence)]
+
+    scores = bleurt_scorer.score(target_sentence, pred_sentence)
+
+    assert type(scores) == list and len(scores) == 1
+
+    return scores[0]
